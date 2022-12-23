d0d412720f0991448509bcc206379c944c11b3ed
Husain Fadhilah <husainazkas@gmail.com>
2022-12-23 23:22:21 +0700

    config routing

---
+++ lib/presentation/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'routes/router.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routeInformationParser: router.routeInformationParser,
+      routeInformationProvider: router.routeInformationProvider,
+      routerDelegate: router.routerDelegate,
+    );
+  }
+}
