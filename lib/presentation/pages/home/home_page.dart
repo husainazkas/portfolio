18227af1dfab86e58142a816ab83f0008dd01ded
Husain Fadhilah <husainazkas@gmail.com>
2022-12-24 01:21:36 +0700

    add sidebar

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -1,14 +1,60 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/side_bar.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  static const _sections = <SideBarMenuItem>[
+    SideBarMenuItem(label: 'About'),
+    SideBarMenuItem(label: 'Skills'),
+    SideBarMenuItem(label: 'Projects'),
+    SideBarMenuItem(label: 'Work Experience'),
+    SideBarMenuItem(label: 'Education'),
+    SideBarMenuItem(label: 'Contact'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello World'),
-      ),
+    return LayoutBuilder(
+      builder: (context, constaints) {
+        final sideBarWidth =
+            constaints.maxWidth > 800 ? 300.0 : constaints.maxWidth * .35;
+        final isMobileSize = constaints.maxWidth < 600;
+        return Scaffold(
+          drawer: isMobileSize
+              ? SideBar(
+                  width: 300.0,
+                  items: _sections,
+                )
+              : null,
+          body: Row(
+            children: [
+              if (!isMobileSize)
+                SideBar(
+                  width: sideBarWidth,
+                  items: _sections,
+                ),
+              Expanded(
+                child: Stack(
+                  children: [
+                    const Center(
+                      child: Text('Hello World'),
+                    ),
+                    if (isMobileSize)
+                      Builder(
+                        builder: (context) => IconButton(
+                          onPressed: () => Scaffold.of(context).openDrawer(),
+                          icon: const Icon(Icons.menu),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
---
+++ lib/presentation/pages/home/widgets/side_bar.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class SideBar extends StatelessWidget {
+  const SideBar({
+    super.key,
+    required this.items,
+    this.width,
+    this.onTap,
+  }) : assert(items.length > 0);
+
+  final List<SideBarMenuItem> items;
+  final ValueChanged<int>? onTap;
+  final double? width;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Drawer(
+      width: width,
+      backgroundColor: theme.colorScheme.primaryContainer,
+      child: Center(
+        child: TextButtonTheme(
+          data: TextButtonThemeData(
+            style: TextButton.styleFrom(
+              fixedSize: Size.fromWidth((width ?? 304) * .8),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 24.0,
+                vertical: 20.0,
+              ),
+            ),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: List.generate(
+              items.length,
+              (index) => TextButton(
+                onPressed: () => onTap?.call(index),
+                child: Text(
+                  items[index].label,
+                  style: items[index].textStyle,
+                  maxLines: 1,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SideBarMenuItem {
+  final String label;
+  final TextStyle? textStyle;
+
+  const SideBarMenuItem({required this.label, this.textStyle});
+}
