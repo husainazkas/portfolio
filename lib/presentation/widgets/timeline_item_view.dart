279fc78c0bc040500d7e64baedc0359bc79ece4c
Husain Fadhilah <husainazkas@gmail.com>
2023-03-24 08:39:02 +0700

    Fix timeline connector not showing

--- lib/presentation/widgets/timeline_item_view.dart
+++ lib/presentation/widgets/timeline_item_view.dart
@@ -20,68 +20,79 @@ class TimelineItemView extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Align(
-      heightFactor: 1.0,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ConstrainedBox(
-            constraints: const BoxConstraints(minWidth: 8.0),
-            child: TimelineNode(
-              indicator: Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 8.0,
+      child: IntrinsicHeight(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ConstrainedBox(
+              constraints: const BoxConstraints(minWidth: 8.0),
+              child: TimelineNode(
+                direction: Axis.vertical,
+                indicator: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 8.0,
+                  ),
+                  child: Indicator.dot(
+                    size: 8.0,
+                    color: theme.colorScheme.onBackground.withOpacity(.8),
+                  ),
                 ),
-                child: Indicator.dot(
-                  size: 8.0,
-                  color: theme.colorScheme.onBackground.withOpacity(.8),
-                ),
+                indicatorPosition: 0.0,
+                startConnector: showStartLine
+                    ? Connector.solidLine(
+                        color: theme.colorScheme.onBackground.withOpacity(.35),
+                      )
+                    : null,
+                endConnector: showEndLine
+                    ? Connector.solidLine(
+                        color: theme.colorScheme.onBackground.withOpacity(.35),
+                      )
+                    : null,
               ),
-              indicatorPosition: 0.0,
-              startConnector: showStartLine
-                  ? Connector.solidLine(
-                      color: theme.colorScheme.onBackground.withOpacity(.35),
-                    )
-                  : null,
-              endConnector: showEndLine
-                  ? Connector.solidLine(
-                      color: theme.colorScheme.onBackground.withOpacity(.35),
-                    )
-                  : null,
             ),
-          ),
-          Expanded(
-            child: Align(
-              alignment: AlignmentDirectional.centerStart,
-              child: SizedBox(
-                width: double.infinity,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 20.0, top: 4.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(timelineExperience.title),
-                      Text(
-                        '${timelineExperience.period} (${timelineExperience.type})',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w300,
-                          color: Theme.of(context)
-                              .colorScheme
-                              .onBackground
-                              .withOpacity(.75),
+            Expanded(
+              child: Align(
+                alignment: AlignmentDirectional.centerStart,
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0, top: 4.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(timelineExperience.title),
+                        Text(
+                          '${timelineExperience.period} (${timelineExperience.type})',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            color: Theme.of(context)
+                                .colorScheme
+                                .onBackground
+                                .withOpacity(.75),
+                          ),
+                          textScaleFactor: .95,
                         ),
-                        textScaleFactor: .95,
-                      ),
-                      const SizedBox(height: 4.0),
-                      Html(data: timelineExperience.description),
-                      const SizedBox(height: 20.0)
-                    ],
+                        const SizedBox(height: 4.0),
+                        Html(
+                          data: timelineExperience.description,
+                          shrinkWrap: true,
+                          style: {
+                            'body': Style(
+                              margin: Margins.symmetric(vertical: 8.0),
+                              padding: EdgeInsets.zero,
+                            )
+                          },
+                        ),
+                        const SizedBox(height: 20.0)
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
