a213da592b1408119c5e4de3960edf026faacaac
Husain Fadhilah <husainazkas@gmail.com>
2023-10-21 23:35:09 +0700

    fix: invalid text style & add support for no dots

--- lib/presentation/widgets/timeline_item_view.dart
+++ lib/presentation/widgets/timeline_item_view.dart
@@ -46,42 +46,46 @@ class TimelineItemView extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              ConstrainedBox(
-                constraints: const BoxConstraints(minWidth: 8.0),
-                child: TimelineNode(
-                  direction: Axis.vertical,
-                  indicator: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 8.0,
+              if (showStartLine || showEndLine)
+                ConstrainedBox(
+                  constraints: const BoxConstraints(minWidth: 8.0),
+                  child: TimelineNode(
+                    direction: Axis.vertical,
+                    indicator: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 8.0,
+                      ),
+                      child: Indicator.dot(
+                        size: 8.0,
+                        color: textColor ??
+                            theme.colorScheme.onBackground.withOpacity(.8),
+                      ),
                     ),
-                    child: Indicator.dot(
-                      size: 8.0,
-                      color: textColor ??
-                          theme.colorScheme.onBackground.withOpacity(.8),
-                    ),
+                    indicatorPosition: 0.0,
+                    startConnector: showStartLine
+                        ? Connector.solidLine(
+                            color: textColor ??
+                                theme.colorScheme.onBackground.withOpacity(.35),
+                          )
+                        : null,
+                    endConnector: showEndLine
+                        ? Connector.solidLine(
+                            color: textColor ??
+                                theme.colorScheme.onBackground.withOpacity(.35),
+                          )
+                        : null,
                   ),
-                  indicatorPosition: 0.0,
-                  startConnector: showStartLine
-                      ? Connector.solidLine(
-                          color: textColor ??
-                              theme.colorScheme.onBackground.withOpacity(.35),
-                        )
-                      : null,
-                  endConnector: showEndLine
-                      ? Connector.solidLine(
-                          color: textColor ??
-                              theme.colorScheme.onBackground.withOpacity(.35),
-                        )
-                      : null,
                 ),
-              ),
               Expanded(
                 child: Align(
                   alignment: AlignmentDirectional.centerStart,
                   child: SizedBox(
                     width: double.infinity,
                     child: Padding(
-                      padding: const EdgeInsets.only(left: 20.0, top: 4.0),
+                      padding: EdgeInsets.only(
+                        left: showStartLine || showEndLine ? 20.0 : 0.0,
+                        top: 4.0,
+                      ),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
@@ -95,24 +99,30 @@ class TimelineItemView extends StatelessWidget {
                             ),
                           if (data.description != null) ...[
                             const SizedBox(height: 4.0),
-                            Html(
-                              data: data.description,
-                              style: {
-                                'body': Style(
-                                  margin: Margins.symmetric(vertical: 8.0),
-                                  padding: HtmlPaddings.zero,
-                                ),
-                                ...HtmlStyle.getEffectiveTextStyle(
-                                  h1TextStyle: h1TextStyle,
-                                  h2TextStyle: h2TextStyle,
-                                  h3TextStyle: h3TextStyle,
-                                  h4TextStyle: h4TextStyle,
-                                  h5TextStyle: h5TextStyle,
-                                  h6TextStyle: h6TextStyle,
-                                  textStyle: textStyle,
-                                  textColor: textColor,
-                                ),
-                              },
+                            DefaultTextStyle.merge(
+                              style: theme.textTheme.bodyMedium
+                                  ?.merge(textStyle)
+                                  .copyWith(color: textColor),
+                              child: Html(
+                                data: data.description,
+                                style: {
+                                  'body': Style(
+                                    margin: Margins.symmetric(vertical: 8.0),
+                                    padding: HtmlPaddings.zero,
+                                  ),
+                                  'ul': Style(margin: Margins.zero),
+                                  ...HtmlStyle.getEffectiveTextStyle(
+                                    h1TextStyle: h1TextStyle,
+                                    h2TextStyle: h2TextStyle,
+                                    h3TextStyle: h3TextStyle,
+                                    h4TextStyle: h4TextStyle,
+                                    h5TextStyle: h5TextStyle,
+                                    h6TextStyle: h6TextStyle,
+                                    textStyle: textStyle,
+                                    textColor: textColor,
+                                  ),
+                                },
+                              ),
                             ),
                           ],
                           const SizedBox(height: 20.0)
